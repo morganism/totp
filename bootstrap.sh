9e0689bcaec1dca8303888944197a95c9f882031
Morgan Sziraki <morgan.sziraki@gmail.com>
2026-06-18 12:24:25 +0100

    link file in bin to ~/bin

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,36 @@
 #!/usr/bin/env bash
+: <<DOCXX
+Running this after cloning will setup what this repo provides
+The idea is each of my repos will have a bootstrap.sh file if appropriate.
+https://raw.githubusercontent.com/morganism/totp/refs/heads/master/bootstrap.sh
+Author: morgan@morganism.dev
+Date: Thu 18 Jun 2026 07:26:43 BST
+DOCXX
 
+#---- for x-repo bootstrapping if implemented
+#     hopes to solve dependency issues
 declare -a MISSING_REPOS
 declare -a MISSING_MESSAGES
-
 if [[ -f ~/.ansi.functions ]]; then
-  . ~/.ansi.functions
+  . ~/.ansi.functions                                  # created by the linker in tools 
 else
   MISSING_REPOS+="tools"
-  MISSING_MESSAGES+="Install Tools Repo for a Bootstrapper and dotfiles ?"
+  MISSING_MESSAGES+="Install Tools Repo for dotfiles"
 fi
-echo -e "🧚 Help on the way"
 
+echo -e "🧚 ${Grey_8}Linking files ...${Reset}"
 
-echo -e "🛟 "
+THIS=$(realpath $0)
+THIS_DIR=$(dirname $THIS)
+FILES_TO_LINK=$(find $THIS_DIR/bin -type f -exec realpath {} \;)
 
-echo " I am : https://raw.githubusercontent.com/morganism/totp/refs/heads/master/bootstrap"
+for f in "$FILES_TO_LINK"
+do
+  BN=$(basename $f)
+  printf "${Grey_8}Linking ${White}%s${Reset}\n" "${BN}"
+  ln -fs $f ~/bin/$BN
+done
+
+printf "${Green}%s${Reset}\n" "OK"
+
 
